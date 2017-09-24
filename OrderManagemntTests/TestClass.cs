3c158fdd28178eb6eb654e3bbb569159397794e6
reddy.pavankumarr@gmail.com <rpavankumar-reddy>
2017-09-24 18:28:20 +0100

    Added Files2 2

---
+++ OrderManagemntTests/TestClass.cs
@@ -0,0 +1,242 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagementAPI.Models;
+using OrderManagementAPI.Custom;
+
+namespace OrderManagementTests
+{
+    //[TestFixture]
+    [SetUpFixture]
+    public class TestClass
+    {
+        ManageOrders orderManage;
+
+        List<OrderItems> orderItems = new List<OrderItems>
+        {
+            new OrderItems
+            {
+                OrderId = 11,
+                CustomerName = "MarkTest",
+                Phone = "7777666662",
+                Address = "London",
+                ItemBasketList = new List<Items>
+                {
+                        new Items { ItemName = "Iphone8",ItemQuantity = 2 },
+                        new Items { ItemName = "GalaxyNote",  ItemQuantity = 1 }
+                }
+            }
+        };
+        OrderItems addOrderItem = new OrderItems
+        {
+            OrderId = 3,
+            CustomerName = "TestOrder",
+            Phone = "7777666662",
+            Address = "Birmingham",
+            ItemBasketList = new List<Items>
+            {
+                    new Items { ItemName = "Apple",ItemQuantity = 5 }
+            }
+
+        };
+        OrderItems updateOrder = new OrderItems
+        {
+            OrderId = 11,
+            CustomerName = "MarkTest",
+            Phone = "777766666",
+            Address = "Kensington",
+            ItemBasketList = new List<Items>
+            {
+                    new Items { ItemName = "Apple",ItemQuantity = 5 }
+            }
+
+        };
+        [SetUp]
+        public void SetUp()
+        {
+            orderManage = new ManageOrders(orderItems);
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+
+        [Test()]
+        public void GetAllOrdersTest()
+        {
+            ManageOrders order = new ManageOrders();
+            var expected = order.GetAllOrders();
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void GetOrderTest()
+        {
+            OrderItems expected = orderManage.GetOrder(11);
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void AddCustomerOrderTest()
+        {
+            OrderItems expected = orderManage.AddCustomerOrder(addOrderItem);
+            var exp = orderManage.GetAllOrders();
+            Assert.AreEqual(expected.OrderId, addOrderItem.OrderId);
+        }
+
+        [Test()]
+        //[ExpectedException(typeof(ArgumentNullException))]
+        public void AddNullCustomerOrderTest()
+        {
+            //OrderItems expected = orderManage.AddCustomerOrder(null);
+
+            var exception = Assert.Catch(() => orderManage.AddCustomerOrder(null));
+            Assert.IsInstanceOf<Exception>(exception);
+        }
+
+        [Test()]
+        public void AddExistingCustomerOrderTest()
+        {
+            try
+            {
+                OrderItems expected = orderManage.AddCustomerOrder(orderItems.First());
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("CustomerOrder already exists", ex.Message);
+            }
+        }
+
+        [Test()]
+        public void UpdateCustomerOrderTest()
+        {
+            OrderItems expected = orderManage.UpdateCustomerOrder(updateOrder);
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void UpdateNullCustomerOrderTest()
+        {
+            //OrderItems expected = orderManage.UpdateCustomerOrder(null);
+            var exception = Assert.Catch(() => orderManage.UpdateCustomerOrder(null));
+            Assert.IsInstanceOf<Exception>(exception);
+
+        }
+
+        [Test()]
+       // [ExpectedException(typeof(Exception))]
+        public void UpdateInvalidCustomerOrderTest()
+        {
+            //OrderItems expected = orderManage.UpdateCustomerOrder(new OrderItems());
+            var exception = Assert.Catch(() => orderManage.UpdateCustomerOrder(new OrderItems()));
+            Assert.IsInstanceOf<Exception>(exception);
+        }
+
+        [Test()]
+        public void DeleteCustomerOrderTest()
+        {
+            OrderItems expected = orderManage.DeleteCustomerOrder(11);
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void DeleteInvalidCustomerOrderTest()
+        {
+            OrderItems expected = orderManage.DeleteCustomerOrder(3);
+            Assert.IsNull(expected);
+        }
+
+        [Test()]
+        public void AddOrderItemsTest()
+        {
+            OrderItems expected = orderManage.AddOrderItems(11, "HTCDesire", 2);
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void AddInvalidOrderIdItemsTest()
+        {
+            OrderItems expected = orderManage.AddOrderItems(4, "HTCDesire", 2);
+            Assert.IsNull(expected);
+        }
+
+        [Test()]
+        public void AddOrderItemsExistTest()
+        {
+            try
+            {
+                OrderItems expected = orderManage.AddOrderItems(11, "Iphone8", 2);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Item already exists", ex.Message);
+            }
+        }
+
+        [Test()]
+        public void UpdateOrderItemsTest()
+        {
+            OrderItems expected = orderManage.UpdateOrderItems(11, "GalaxyNote", 20);
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void UpdateInvalidOrderItemsTest()
+        {
+            OrderItems expected = orderManage.UpdateOrderItems(4, "GalaxyNote", 20);
+            Assert.IsNull(expected);
+        }
+
+        [Test()]       
+        public void UpdateOrderItemsNotExistTest()
+        {                    
+            var exception = Assert.Catch(() => orderManage.UpdateOrderItems(11, "Galaxy", 20));
+            Assert.IsInstanceOf<Exception>(exception);
+
+            //Assert.Throws<ArgumentNullException>(() => orderManage.UpdateOrderItems(11, "Galaxy", 20)());
+        }
+
+        [Test()]
+        public void RemoveOrderItemsTest()
+        {
+            OrderItems expected = orderManage.RemoveOrderItems(11, "GalaxyNote");
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void RemoveInvalidOrderItemsTest()
+        {
+            OrderItems expected = orderManage.RemoveOrderItems(4, "GalaxyNote");
+            Assert.IsNull(expected);
+        }
+
+        [Test()]
+       // [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveItemsNotExistTest()
+        {
+            //OrderItems expected = orderManage.RemoveOrderItems(11, "Galaxy");
+            var exception = Assert.Catch(() => orderManage.RemoveOrderItems(11, "Galaxy"));
+            Assert.IsInstanceOf<Exception>(exception);
+        }
+
+        [Test()]
+        public void ClearOrderItemsListTest()
+        {
+            OrderItems expected = orderManage.ClearOrderItemsList(11);
+            Assert.IsNotNull(expected);
+        }
+
+        [Test()]
+        public void ClearInvalidOrderItemsListTest()
+        {
+            OrderItems expected = orderManage.ClearOrderItemsList(5);
+            Assert.IsNull(expected);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,314 @@
-# CutomerOrderAPI
-API which will allow users to add and remove items and change the quantity of the items
+# CustomerOrdersAPI
+
+CI : [![Build status](https://ci.appveyor.com/api/projects/status/p4fxw6guu3vdpo8c?svg=true)](https://ci.appveyor.com/project/rpavankumar-reddy/customerordersapi)
+CodeCoverage: [![codecov](https://codecov.io/gh/rpavankumar-reddy/CustomerOrdersAPI/branch/master/graph/badge.svg)](https://codecov.io/gh/rpavankumar-reddy/CustomerOrdersAPI)
+
+
+
+
+
+API which will allow users to add and remove items and change the quantity of the items. Users can also be able to simply clear out all items from their order and start again.
+
+Additional functionality of Creating Orders has been implemented.
+
+Different  API endpoints are implemented for adding,updating,deleting orders and also for adding, updating and deleting the items in the order.
+
+The following lines describe how to use the api's for different functionality
+
+1.GetAllOrders - To Get all the orders use the 
+below API EndPoint
+
+http://localhost:[port]/api/CustomerOrders/GetAllOrders 
+
+which returns
+
+Output
+------
+
+```
+[
+  {
+    "OrderId": 1,
+    "CustomerName": "Mark",
+    "Phone": "7777766666",
+    "Address": "London",
+    "ItemBasketList": [
+      {
+        "ItemName": "Iphone6",
+        "ItemQuantity": 2
+      },
+      {
+        "ItemName": "Galaxy",
+        "ItemQuantity": 1
+      }
+    ]
+  }
+]
+```
+
+2.GetOrder - To get the particular single order use the API EndPoint
+
+http://localhost:[port]/api/CustomerOrders/GetOrder?orderId=1
+
+Input:
+------
+OrderId
+
+Output:
+-------
+```
+{
+  "OrderId": 1,
+  "CustomerName": "Mark",
+  "Phone": "7777766666",
+  "Address": "London",
+  "ItemBasketList": [
+    {
+      "ItemName": "Iphone6",
+      "ItemQuantity": 2
+    },
+    {
+      "ItemName": "Galaxy",
+      "ItemQuantity": 1
+    }
+  ]
+}
+```
+
+3.AddNewOrder - To add a new order use the API EndPoint
+
+http://localhost:[port]/api/CustomerOrders/PostCustomerOrder 
+
+Input:
+------
+```
+{
+    "OrderId": 2,
+    "CustomerName": "Mark2",
+    "Phone": "7777666662",
+    "Address": "Birmingham",
+    "ItemBasketList": [
+      {
+        "ItemName": "Philips",
+        "ItemQuantity": 2
+      },
+      {
+        "ItemName": "Samsung",
+        "ItemQuantity": 2
+      }
+    ]
+  }
+```
+
+Output:
+------
+```
+  {
+    "OrderId": 2,
+    "CustomerName": "Mark2",
+    "Phone": "7777666662",
+    "Address": "Birmingham",
+    "ItemBasketList": [
+      {
+        "ItemName": "Philips",
+        "ItemQuantity": 2
+      },
+      {
+        "ItemName": "Samsung",
+        "ItemQuantity": 2
+      }
+    ]
+  }
+  ```
+
+
+4.UpdateOrder - to update an existing order use the API Endpoint
+
+http://localhost:[port]/api/CustomerOrders/PutCustomerOrder 
+
+Input:
+-----
+```
+  {
+    "OrderId": 2,
+    "CustomerName": "MarkWaugh",
+    "Phone": "7775556667",
+    "Address": "Houston",
+    "ItemBasketList": [
+      {
+        "ItemName": "Philips",
+        "ItemQuantity": 2
+      },
+      {
+        "ItemName": "Samsung",
+        "ItemQuantity": 2
+      }
+    ]
+  }
+```
+
+Output:
+------
+```
+{
+  "OrderId": 2,
+  "CustomerName": "MarkWaugh",
+  "Phone": "7775556667",
+  "Address": "Houston",
+  "ItemBasketList": [
+    {
+      "ItemName": "Philips",
+      "ItemQuantity": 2
+    },
+    {
+      "ItemName": "Samsung",
+      "ItemQuantity": 2
+    }
+  ]
+}
+```
+
+
+
+5.DeleteOrder - To delete an order use the API
+EndPoint
+
+http://localhost:[port]/api/CustomerOrders/DeleteCustomerOrder?orderId=2 
+
+Input - orderId
+------
+Output - Returns the deleted order
+------
+
+```
+{
+  "OrderId": 2,
+  "CustomerName": "MarkWaugh",
+  "Phone": "7775556667",
+  "Address": "Houston",
+  "ItemBasketList": [
+    {
+      "ItemName": "Philips",
+      "ItemQuantity": 2
+    },
+    {
+      "ItemName": "Samsung",
+      "ItemQuantity": 2
+    }
+  ]
+}
+```
+
+6.AddOrderItems - To add new Items to the order use the API EndPoint
+
+http://localhost:[port]/api/CustomerOrders/PostCustomerOrderItem?orderId=1&itemName=HTC&quantity=5
+
+Input : orderId,itemName,quantity
+-----
+
+Output: Returns the Order with added Item
+------
+
+```
+{
+  "OrderId": 1,
+  "CustomerName": "Mark",
+  "Phone": "7777766666",
+  "Address": "London",
+  "ItemBasketList": [
+    {
+      "ItemName": "Iphone6",
+      "ItemQuantity": 2
+    },
+    {
+      "ItemName": "Galaxy",
+      "ItemQuantity": 1
+    },
+    {
+      "ItemName": "HTC",
+      "ItemQuantity": 5
+    }
+  ]
+}
+```
+
+7.UpdateOrderItems - To update the items(to change the quantiy) in the order use the API Endpoint
+
+http://localhost:[port]/api/CustomerOrders/PutCustomerOrderItem?orderId=1&itemName=HTC&quantity=100
+
+Input : orderId,itemName,quantity
+-----
+
+Output: Returns the Order with updated quantity in the item
+-----
+```
+{
+  "OrderId": 1,
+  "CustomerName": "Mark",
+  "Phone": "7777766666",
+  "Address": "London",
+  "ItemBasketList": [
+    {
+      "ItemName": "Iphone6",
+      "ItemQuantity": 2
+    },
+    {
+      "ItemName": "Galaxy",
+      "ItemQuantity": 1
+    },
+    {
+      "ItemName": "HTC",
+      "ItemQuantity": 100
+    }
+  ]
+}
+```
+
+8.RemoveOrderItems - To delete an item in the order use the API endPoint
+
+http://localhost:[port]/api/CustomerOrders/DeleteCustomerOrderItem?orderId=1&itemName=Galaxy
+
+
+Input : orderId,itemName,quantity
+-----
+
+Output: Returns the Order by removing the item
+-----
+```
+{
+  "OrderId": 1,
+  "CustomerName": "Mark",
+  "Phone": "7777766666",
+  "Address": "London",
+  "ItemBasketList": [
+    {
+      "ItemName": "Iphone6",
+      "ItemQuantity": 2
+    },
+    {
+      "ItemName": "HTC",
+      "ItemQuantity": 100
+    }
+  ]
+}
+```
+
+
+9.ClearItemsList - To delete all the items in the order use the API Endpoint
+
+http://localhost:[port]/api/CustomerOrders/DeleteOrderItemsList?orderId=1
+
+Input : orderId
+-----
+Output: Returns the Order with Empty Items List
+-----
+```
+{
+  "OrderId": 1,
+  "CustomerName": "Mark",
+  "Phone": "7777766666",
+  "Address": "London",
+  "ItemBasketList": []
+}
+```
+
